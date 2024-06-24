a318bcb25781a65b8066bbd8f25ad35a4fe7b45e
Katelynn Emerson <ke101@wellesley.edu>
2024-06-24 11:21:10 -0400

    wrote instructions for parsing input

--- FirstAdventureGameFile.java
+++ FirstAdventureGameFile.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Welcome to the beginning of your adventure game project! Before you begin 
@@ -7,34 +11,59 @@
  * Instructions:
  * Follow the inline comment instructions. In this file, you will aim to 
  * accept user input and split that input into a format that the computer can 
- * understand.
+ * understand. This will occur continuously until the user enters "q".
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author CS200 Instructors
+ * @version 6.24.2024
  */
 public class FirstAdventureGameFile
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class FirstAdventureGameFile
-     */
-    public FirstAdventureGameFile()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main(String[] args) throws IOException {
+        //create new Scanner, which will take input from the keyboard
+        Scanner scan = new Scanner(System.in);
+        //initialize String variable for input:
+        
+        //write a while loop with the condition that input is not q
+        
+            /*
+             * Inside the loop, do the following:
+             * 1. Print ">".
+             * 2. Set input to scan.nextLine().
+             * 3. Check if input is "q". If so, print a thank you message
+             *    and break the loop.
+             * 4. Create a variable words and set it to wordList(input).
+             * 5. Run parseCommand(words).
+             */
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public static ArrayList<String> wordList(String input) {
+        //these are the things that can separate words:
+        String delims = "[\t,.:;?!\"']";
+        //this splits the input String into an array of words that used to 
+        //be separated by the delimiter
+        String[] strArray = input.split(delims);
+        /*
+         * TODO: Initialize an ArrayList of type String and add every element 
+         * of strArray to that list. Return the list.
+         */
+        
+    }
+    
+    public static void parseCommand(ArrayList<String> wordList) {
+        //declared some variables for you
+        String verb;
+        String noun;
+        ArrayList<String> commands = new ArrayList<String>
+            (Arrays.asList("take", "drop"));
+        ArrayList<String> objects = new ArrayList<String>
+            (Arrays.asList("sword", "ring", "drop"));
+        /*
+         * Write an if-else statement that follows the pseudocode:
+         * if wordList has more than two elements, print a message saying only
+         * two commands allowed
+         * else set verb to the first element of wordList and object to the 
+         * second & check that the verb and object are contained in the lists
+         * of commands and objects
+         */
     }
 }
